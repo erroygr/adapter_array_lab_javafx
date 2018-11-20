b85006a9edf6b97f67b2d4b9c090a808556540cb
erroygr <43731453+erroygr@users.noreply.github.com>
2018-11-20 21:22:48 +0300

    Commit 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -22,24 +22,36 @@ public class Controller {
 
     public void met_pop()//добавить
     {
+        String met_pop="Добавить элемент: "+String.valueOf(value.getText())+". \n Массив:";
         demo.insert(Long.valueOf(value.getText()));
+        list_w.getItems().add(met_pop);
         list_w.getItems().add(demo.display());
     }
     public void met_del()//удалить
     {
-        if(demo.delete(Long.valueOf(value.getText()))) {
-            System.out.println(value.toString()+" удален");
-        }else   System.out.println(value.toString()+"не удален или не найден");
+        String met_del="Удалить элемент: "+String.valueOf(value.getText());
+        String met_del_1="";
+        list_w.getItems().add(met_del);
+        if(demo.delete(Long.valueOf(value.getText()))){
+            met_del_1="Элемент удален"+".\n Массив:";
+        }else  met_del_1="Элемент не удален, или его нет в массиве"+".\n Массив:";
 
+            list_w.getItems().add(met_del_1);
         list_w.getItems().add(demo.display());
     }
     public void met_search()//поиск
     {
-        long fK;
-        fK= demo.find(Long.valueOf(value.getText()));
+        String met_search_2="";
+        String met_search_1=" Найти элемент: "+String.valueOf(value.getText());
+        list_w.getItems().add(met_search_1);
+        long val_1= Long.valueOf(demo.find(Long.valueOf(value.getText())));
+         if(val_1!=demo.size()) {
+              met_search_2="Элемент найден. Место элемента в массиве: "+val_1+".\n Массив:";
+         } else {
+              met_search_2 = "Элемент  не найден.\n Массив:";
+         }
 
-            System.out.println(fK+" Найден");
-
+        list_w.getItems().add(met_search_2);
         list_w.getItems().add(demo.display());
     }
 
